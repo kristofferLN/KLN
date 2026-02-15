e4644e6ca2cdb98ba259d4dc1c27a1e64975e1d3
KristofferLN <kristoffer.LN@hotmail.com>
2026-02-15 17:44:57 +0100

    en ændring i urls.py i håbet om at kunne vise png filer på hjemmesiden

--- KLN/urls.py
+++ KLN/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('', base_url, name='base_url'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
